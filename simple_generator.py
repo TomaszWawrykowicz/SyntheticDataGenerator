bbec808b57161e740ad3a0b1593f2838cefa1b68
EnZaNin <246823@student.pwr.edu.pl>
2023-03-24 15:44:50 +0100

    feat: add categorical labels for columns

--- simple_generator.py
+++ simple_generator.py
@@ -1,8 +1,14 @@
+from decimal import Decimal
+
 import numpy as np
 import pandas as pd
 import tqdm
+from numpy import int64, int32, float64
 from sklearn.base import BaseEstimator
 from sklearn.mixture import GaussianMixture
+
+# Option to display all columns
+pd.set_option('display.max_columns', None)
 
 
 class GMMSyntheticDataGenerator(BaseEstimator):
@@ -10,46 +16,66 @@ class GMMSyntheticDataGenerator(BaseEstimator):
     def __init__(self):
         self.n_features = None
         self.n_samples = None
+        self.original_data = None
         self.data = None
         self.model = None
         self.factorize = []
+        self.original_dtypes = None
+        self.dtypes = None
+        self.names = None
 
-    def preproccess_data(self):
-        for (columnName, columnData) in self.data.items():
+    def preproccess_data(self, data):
+        self.original_dtypes = data.dtypes.to_dict()
+        self.names = self.original_dtypes.keys()
+
+        for (columnName, columnData) in data.items():
 
             if columnData.dtype == object:
-                factorized_data, indexes = pd.factorize(self.data[columnName])
-                self.data[columnName] = factorized_data
+                factorized_data, indexes = pd.factorize(data[columnName])
+                data[columnName] = factorized_data
                 self.factorize.append((columnName, {i: indexes[i] for i in range(len(indexes))}))
+
+        self.dtypes = data.dtypes.to_dict()
         print('Data preproccessed')
         print(self.factorize)
 
     def postprocess_data(self, rounding=False, to_integers=False, mapping=False):
         # Rounding and mapping original data naming
-        # Rounding
-        # if int -> int, dec = 0, factorize niżej, float nie wiem
-        if rounding:
-            pass
 
+        # Rounding
+        # if int -> int, dec = 0, factorize niżej, float
+        if rounding:
+            for key, dtype in self.dtypes.items():
+                if dtype in [int64, int32]:
+                    self.data[key] = self.data[key].round(0).astype('int64')
+                elif dtype == float64:
+                    dec = max([abs(Decimal(str(x)).as_tuple().exponent)
+                               for x in self.original_data[key].sample(10).to_list()])
+                    self.data[key] = self.data[key].round(dec).astype('float64')
+                elif dtype == object and to_integers:
+                    self.data[key] = self.data[key].round(0).astype('int64')
+
+        # Restoring discrete values
         for column in self.factorize:
-            print(self.data[column[0]])
-            print('klucze', list(column[1].keys()))
-            #to_integers
-            if to_integers:
-                pass
+            # print(self.data[column[0]])
+            # print('klucze', list(column[1].keys()))
+
+            lenght = len(column[1])
+            self.data[column[0]] = self.data[column[0]].apply(
+                lambda x: 0 if x < 0 else (lenght - 1 if x >= lenght else x))
 
             # Mapping back
             if mapping:
                 self.data[column[0]] = self.data[column[0]].map(column[1])
-            print(self.data[column[0]])
-        print('Mapped original data types')
+                # print(self.data[column[0]])
+                print('Mapped original data types')
 
     def fit(self, x):
-        self.data = x
+        # self.data = x
+        self.original_data = x
 
-        self.preproccess_data()
-        self.postprocess_data()
-        return False
+        self.preproccess_data(self.original_data)
+        # self.postprocess_data()
         print('Start building model')
         # print(type(self.data))
 
@@ -59,9 +85,9 @@ class GMMSyntheticDataGenerator(BaseEstimator):
         n_components_range = range(1, 41)
 
         for n in tqdm.tqdm(n_components_range):
-            model = GaussianMixture(n, covariance_type='full').fit(self.data)
+            model = GaussianMixture(n, covariance_type='full').fit(self.original_data)
             models.append(model)
-            bic_scores.append(model.bic(self.data))
+            bic_scores.append(model.bic(self.original_data))
 
         best_n_components = n_components_range[np.argmin(bic_scores)]
         print('Number of components:', best_n_components, 'based on Bayes Information criterion')
@@ -76,7 +102,14 @@ class GMMSyntheticDataGenerator(BaseEstimator):
 
     def sample(self, n_sample):
         if self.model:
-            return self.model.sample(n_sample)[0]
+            self.data = pd.DataFrame(self.model.sample(n_sample)[0], columns=self.names)
+            # self.postprocess_data(rounding=True, to_integers=False, mapping=False)
+            self.postprocess_data(rounding=True, to_integers=True, mapping=True)
+            print(self.original_data.info())
+            print(self.data.info())
+            print(self.original_data.describe())
+            print(self.data.describe())
+            return self.data
         else:
             raise TypeError('Model not build')
 
@@ -94,7 +127,7 @@ class GMMSyntheticDataGenerator(BaseEstimator):
 
 
 gen = GMMSyntheticDataGenerator()
-data = pd.read_csv('heart_uci.csv')
-gen.fit(data)
-# syn_data = gen.sample(100)
-# print(syn_data)
+kaggle_data = pd.read_csv('heart_uci.csv')
+gen.fit(kaggle_data)
+syn_data = gen.sample(100)
+print(syn_data)
